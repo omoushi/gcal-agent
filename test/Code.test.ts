34680c70a8cbe54479f727b2c113dd9063d4d315
h-haraguchi <account.cgtdnpck@me.com>
2019-04-30 00:46:11 +0900

    extract script properties as json

--- test/Code.test.ts
+++ test/Code.test.ts
@@ -1,10 +1,12 @@
 import {doPost} from "../src/Code"
 import exampleEventParameter from "./resources/example_event_parameter.json"
+import exampleScriptProperties from "./resources/example_script_properties.json"
 import {ScriptProps, SlackEventParameter} from "../src/typings"
 import * as properties from '../src/properties'
 import * as verifier from '../src/verifier'
 
 const e: SlackEventParameter = <SlackEventParameter>exampleEventParameter
+const scriptProps: ScriptProps = <ScriptProps>exampleScriptProperties
 
 jest.mock('../src/properties');
 const mockScriptProperties = <jest.Mock<ScriptProps>>properties.scriptProperties
@@ -12,7 +14,7 @@ jest.mock('../src/verifier');
 const mockVerify = <jest.Mock<ScriptProps>>verifier.verify
 
 describe('doPost', () => {
-    beforeEach(() => mockScriptProperties.mockReturnValue({calendar_id: '', slack_token: '', slack_signed_secret: ''}))
+    beforeEach(() => mockScriptProperties.mockReturnValue(scriptProps))
 
     it('executes without causing exception', () => doPost(e))
 
